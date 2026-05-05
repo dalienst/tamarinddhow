bcd499da32538a4f9e6585de93394f7d19462c62
Dalienst <oduordalienst@gmail.com>
2026-05-05 19:41:54 +0300

    update accounts

--- app/(private)/dhow-manager/dashboard/page.tsx
+++ app/(private)/dhow-manager/dashboard/page.tsx
@@ -1,4 +1,10 @@
+"use client"
+
+import { useFetchAccount } from "@/hooks/accounts/actions"
+
 export default function DhowManagerDashboard() {
+    const { data: account, isLoading, error } = useFetchAccount()
+    console.log(account, isLoading, error)
     return (
         <div>
             <h1>Dhow Manager Dashboard</h1>

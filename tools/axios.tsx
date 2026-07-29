bca3373ebe319a74de4ce79c6e59758867aff685
Dalienst <oduordalienst@gmail.com>
2026-07-29 17:20:44 +0300

    update axios

--- tools/axios.tsx
+++ tools/axios.tsx
@@ -14,18 +14,7 @@ export const apiActions: AxiosInstance = axios?.create({
     withCredentials: true,
 });
 
-apiActions.interceptors.request.use((config) => {
-    if (config.method === 'get') {
-        config.params = {
-            ...config.params,
-            _t: Date.now(),
-        };
-    }
-    return config;
-});
-
 export const apiMultipartActions: AxiosInstance = axios?.create({
     baseURL: BASE_URL,
     withCredentials: true,
 });
-

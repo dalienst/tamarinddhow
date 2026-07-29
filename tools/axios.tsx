7f02f863f80f5533b1514de9a6e45ede193656f4
Dalienst <oduordalienst@gmail.com>
2026-07-29 17:18:16 +0300

    Fix loading

--- tools/axios.tsx
+++ tools/axios.tsx
@@ -10,17 +10,13 @@ export default axios?.create({
 
 export const apiActions: AxiosInstance = axios?.create({
     baseURL: BASE_URL,
-    headers: { "Content-Type": "application/json" },
+    headers: { 
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Pragma": "no-cache",
+        "Expires": "0"
+    },
     withCredentials: true,
-});
-
-apiActions.interceptors.request.use((config) => {
-    if (config.method === 'get') {
-        config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-        config.headers['Pragma'] = 'no-cache';
-        config.headers['Expires'] = '0';
-    }
-    return config;
 });
 
 export const apiMultipartActions: AxiosInstance = axios?.create({
